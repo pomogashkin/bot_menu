daeff3a908a76ae627d1b1f12fbfd954944c2bfc
Данил Носков <noskovdaniks@gmail.com>
2023-03-02 12:28:32 +0300

    gooo

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,5 +21,5 @@ echo "Collecting static ..."
 python manage.py collectstatic
 
 # Start server
-echo "Starting server ..."
-python manage.py runserver 0.0.0.0:8000
+# echo "Starting server ..."
+# python manage.py runserver 0.0.0.0:8000
